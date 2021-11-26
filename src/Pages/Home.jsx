7c37fc19b167cb7a7713f1c4c96762facbcbb968
Chidera Okeke <okekechidera97@gmail.com>
2021-11-26 13:16:08 +0100

    worked on page transitions

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,78 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Header from "../components/Header";
 import Loading from "../components/Loading";
+import gsap, { Power3, Power4 } from "gsap";
 
 function Home() {
-	return (
-		<div className="header">
-			<Header />
-			<Loading />
-		</div>
-	);
+  let screen = useRef(null);
+  let body = useRef(null);
+
+  useEffect(() => {
+    var tl = gsap.timeline();
+    tl.to(screen, {
+      duration: 2.5,
+      height: "100%",
+      ease: Power3.easeInOut,
+    });
+    tl.to(screen, {
+      duration: 2.5,
+      left: "100%",
+      ease: Power3.easeInOut,
+      delay: 0.3,
+    });
+    tl.set(screen, { left: "-100%" });
+    gsap
+      .to(body, {
+        duration: 3,
+        css: {
+          opacity: "0.99",
+          pointerEvents: "auto",
+          ease: Power4.easeInOut,
+        },
+      })
+      .delay(4.5);
+    return () => {
+      gsap.to(body, {
+        duration: 3,
+        css: {
+          opacity: "0",
+          pointerEvents: "none",
+        },
+      });
+    };
+  });
+
+  return (
+    <div className="home">
+      <div className="home">
+        <div className="load-container">
+          <div
+            className="load-screen1"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            ref={(el) => (screen = el)}
+          >
+            <h1
+              style={{
+                backgroundColor: "transparent",
+              }}
+            >
+              WELCOME, PRECIOUS
+            </h1>
+          </div>
+        </div>
+        <div className="headd" ref={(el) => (body = el)}>
+          <div>
+            <Header />
+            <Loading />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <span>
+        {" "}
+        Made with &#x2764;&#xfe0f; by{" "}
+        <a
+          href="https://dhera.dev/"
+          target="_blank"
+          rel="noreferrer"
+          style={{ color: "#fff", textDecoration: "underline" }}
+        >
+          Chidera Okeke
+        </a>
+      </span>
+    </div>
+  );
+}
+
+export default Footer;
